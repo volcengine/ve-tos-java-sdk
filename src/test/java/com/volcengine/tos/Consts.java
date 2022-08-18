d6c2237a32accea1fbef6d445b659a1f13450fb6
chenyijian.yisiy <chenyijian.yisiy@bytedance.com>
2022-08-18 19:51:15 +0800

    fix NPE in TosRequest

--- src/test/java/com/volcengine/tos/Consts.java
+++ src/test/java/com/volcengine/tos/Consts.java
@@ -8,4 +8,10 @@ import org.slf4j.LoggerFactory;
 public class Consts {
     public static final Logger LOG = LoggerFactory.getLogger(Consts.class);
     public static final ObjectMapper JSON = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    public static final String bucket = "";
+    public static final String endpoint = "";
+    public static final String region = "";
+    public static final String accessKey = "";
+    public static final String secretKey = "";
+    public static final String bucketCopy = "";
 }
